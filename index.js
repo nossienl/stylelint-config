b6c2c0588dcdb1e6228602fecc8a7ea092131bd0
Niels Otterspoor <niels.otterspoor@gmail.com>
2022-05-04 17:04:39 +0200

    intial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["stylelint-config-standard", "stylelint-config-idiomatic-order"],
+  plugins: ["stylelint-scss"],
+  rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    "max-empty-lines": 1,
+    "number-leading-zero": "always",
+    "declaration-block-single-line-max-declarations": 0,
+    "color-hex-case": "upper",
+    "color-hex-length": "long",
+    "color-function-notation": "modern",
+    "alpha-value-notation": "number",
+    "unit-disallowed-list": ["s"],
+    "max-nesting-depth": [
+      2,
+      {
+        ignoreAtRules: ["media"],
+        ignore: ["pseudo-classes"],
+      },
+    ],
+  },
+};
